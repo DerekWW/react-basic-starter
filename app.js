65229e5b8e9aa838b87a27d93861b6d2e10037ae
DerekWW <derek.wierson@gmail.com>
2018-06-13 20:51:50 -0700

    add React to client index.js

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
 
 app.get('*', (req, res, next) => {
-  res.sendFile(path.join(__dirname+'dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
 app.listen(PORT, () => {
--- src/client/components/App/App.js
+++ src/client/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './App.scss'
 
 export default class App extends React.Component {
