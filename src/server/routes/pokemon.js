2158a3b9b3222d59dd33085faf5340aeb83b2cd2
derekww <derek.wierson@gmail.com>
2018-06-22 23:49:52 -0700

    add comment to pokemon.js

--- src/server/routes/pokemon.js
+++ src/server/routes/pokemon.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const axios = require('axios') 
 
+//this route would be avliable at /api/pokemon/ID
 router.get('/pokemon/:id', (req, res, next) => {
   let id = req.params.id;
 
